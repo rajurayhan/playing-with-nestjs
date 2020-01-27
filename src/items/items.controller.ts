3f5cbad4ccdfdab623db739cd28a0857ede2585a
Raju Rayhan <raju.rayhan@yandex.com>
2020-01-27 18:23:30 +0600

    Minor Changes

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { 
+    Controller, 
+    Get, 
+    Post, 
+    Put, 
+    Delete,
+    Body, 
+    Param 
+}  from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto'
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+    @Get()
+    findAll(): string{
+        return 'Get all items';
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id): string{
+        return `Item ${id}`;
+    }
+
+    @Post()
+    createItem(@Body() creatItemDto: CreateItemDto): string {
+        // return 'Create item'; 
+        return  `Name: ${creatItemDto.name}`;
+    }
+}
